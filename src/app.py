f89d78dae98c04247c10532f90af20050a25da5b
wizard1209 <wizard1209@pm.me>
2024-02-21 18:39:52 -0300

    hello world but now its bot

--- src/app.py
+++ src/app.py
@@ -1,8 +1,32 @@
+from pathlib import Path
 import sys
 
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters import CommandStart
+from aiogram.utils.markdown import hbold
 
-# TODO: probably load api_key on module level
+def load_api_key(api_key_path: Path):
+    if not api_key_path.exists():
+        raise FileNotFoundError("API key path does not exist")
+
+    return api_key_path.read_text()
+
+
+def init_handlers(dp: Dispatcher):
+    @dp.message(CommandStart())
+    async def command_start_handler(message: Message) -> None:
+        await message.answer(f"Hello, {hbold(getattr(message.from_user, 'full_name', 'noname'))}!")
+
+
+async def main(bot: Bot) -> None:
+    dp = Dispatcher()
+    init_handlers(dp)
+    await dp.start_polling(bot)
 
 
 if __name__ == '__main__':
-    print("Hello world")
+    if len(sys.argv) < 2:
+        raise RuntimeError("Path for API key was not provided")
+    api_key = load_api_key(Path(sys.argv[1]))
+    bot = Bot(api_key)
